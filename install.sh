b81cec4f6d707ee74c7b9a6aad6f3c2305324626
nickolanack <nickblackwell82@gmail.com>
2016-05-03 16:36:04 -0700

    add install scripts

---
+++ install.sh
@@ -0,0 +1,38 @@
+rm html/* -rf
+if [ -d 'application/application' ]; then
+   echo 'Fetch codeigniter repository'
+   rm codeigniter -rf
+   mkdir tmp
+   git clone -b 2.2-stable https://github.com/bcit-ci/CodeIgniter.git codeigniter
+   mv -f codeigniter/* tmp/
+   rsync -rv application/ tmp/
+   mv tmp application/html
+fi
+echo 'Move application to document root'
+mv application/html/* html/
+cp application/html/.htaccess html/
+echo 'Replace mysql primary user in config'
+
+echo "\$db['default']['dbdriver']='mysqli';" >> html/application/config/database.php
+echo "\$db['default']['username']='testuser';" >> html/application/config/database.php
+echo "\$db['default']['database']='qb2014';" >> html/application/config/database.php
+echo "\$db['default']['password']='duke851';" >> html/application/config/database.php
+
+echo "\$active_record=TRUE;" >> html/application/config/database.php
+$active_record
+
+
+echo "\$config['encryption_key'] = 'FTPCLASS';" >> html/application/config/config.php
+echo "\$config['cookie_secure'] = FALSE;" >> html/application/config/config.php
+echo "\$config['base_url'] = 'http://'.\$_SERVER['SERVER_NAME'].':'.\$_SERVER['SERVER_PORT'];" >> html/application/config/config.php
+
+
+echo 'Remove apache redirect to https directive'
+cp html/.htaccess html/htaccess.bak
+sed -i '8d' html/.htaccess
+sed -i '8d' html/.htaccess
+sed -i '8d' html/.htaccess
+
+echo 'Override production mode detection'
+sed -i 's/if (custom_/if(true||custom_/' html/index.php
+sed -i 's/if(custom_/if(true||custom_/' html/index.php
---
+++ updatedb.sh
@@ -0,0 +1,57 @@
+for i in *.qb2014.sql.gz; do
+
+  echo $i      
+  /bin/gzip -d $i
+
+  for j in *.qb2014.sql; do
+
+
+    echo 'create database qb2014;' | mysql
+
+    echo 'create user testuser@"localhost" identified by "duke851";' | mysql
+    echo 'grant all on qb2014.* to testuser@"localhost";' | mysql
+
+    #echo 'show databases;' | mysql
+
+    mysql qb2014 < $j
+
+
+    echo 'ALTER FUNCTION bankstatementbanktrans SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION finddborcr SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION findvendordrorcr SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION findvendormatchingbankstatement SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION gettotal SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION hidematchedbankitem SQL SECURITY INVOKER;' | mysql qb2014
+    echo 'ALTER FUNCTION vendorinvoicebanktrans SQL SECURITY INVOKER;' | mysql qb2014
+    
+
+  done
+
+done
+
+for i in *_*.gz; do
+
+  echo $i
+  /bin/gzip -d $i
+
+  name=${i%.*}
+  name=${name%.*}
+  name=${name#*.}
+  echo $name
+
+
+  echo "create database ${name};"
+  echo "create database ${name};" | mysql
+
+  echo "grant all on ${name}.* to testuser@'localhost';"
+  echo "grant all on ${name}.* to testuser@'localhost';" | mysql
+
+  echo "create user ${name}@'localhost' identified by '2pOBrNli';"
+  echo "create user ${name}@'localhost' identified by '2pOBrNli';" | mysql
+
+  echo "grant all on ${name}.* to ${name}@'localhost';"
+  echo "grant all on ${name}.* to ${name}@'localhost';" | mysql
+
+  for j in *${name}*sql; do
+        mysql "${name}" < "${j}"
+  done
