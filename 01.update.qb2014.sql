3bd60a45751cf04b632b7e03985f9dcb20f9e064
nickolanack <nickblackwell82@gmail.com>
2016-05-09 00:50:51 -0700

    add db structure update

---
+++ 01.update.qb2014.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `save_memorize_data` ADD `itemmarkuppercentage` VARCHAR( 10 ) NULL DEFAULT NULL;
+ALTER TABLE `vendor_tbl` ADD `itemmarkuppercentage` VARCHAR( 10 ) NULL DEFAULT NULL;
+ALTER TABLE `vendor_items` ADD `itemmarkuppercentage` VARCHAR( 10 ) NULL DEFAULT NULL;
+ALTER TABLE `bank_feed` ADD `itemmarkuppercentage` VARCHAR( 10 ) NULL DEFAULT NULL;
